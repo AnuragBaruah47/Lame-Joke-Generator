0f67ac7d92642f7dbf7736dd3b4ba9adabfc4d31
AnuragBaruah47 <anuragbaruah2004@gmail.com>
2025-02-12 20:41:57 +0530

    Unit Change Px to Viewport

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const button = document.querySelector(".button");
 const setup = document.querySelector(".setup");
 const punchline = document.querySelector(".punchline");
-const baseURL1 = "https://sv443.net/jokeapi/v2/joke/Any";
+const baseURL1 = "https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Pun,Spooky,Christmas?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
 const baseURL2 = "https://v2.jokeapi.dev/joke/Dark";
 const toggleButton=document.querySelector(".theme-checkbox")
 const firstHalf=document.querySelector(".firsthalf")
@@ -27,9 +27,11 @@ if (toggleButton.checked===false){
       button.disabled = false;
       setup.innerHTML = `<h3>${data.joke}</h3>`
       punchline.innerHTML = `<h3></h3>`
+      console.log("Light Jokes");
     } else {
       setup.innerHTML = `<h3>${data.setup}</h3>`;
       button.disabled = true;
+      console.log("Light Jokes");
       setTimeout(function () {
         punchline.innerHTML = `<h3>${data.delivery}</h3>`;
         button.disabled = false;
@@ -42,15 +44,15 @@ if (toggleButton.checked===false){
     try {
       const response = await fetch(baseURL2);
       const data = await response.json();
-      console.log(data.setup || data.joke);
-      console.log(data.type)
       if (data.type === "single") {
         button.disabled = false;
         setup.innerHTML = `<h3>${data.joke}</h3>`
         punchline.innerHTML = `<h3></h3>`
-      } else {
+        console.log("Dark Jokes");
+      }else{
         setup.innerHTML = `<h3>${data.setup}</h3>`;
         button.disabled = true;
+        console.log("Dark Jokes");
         setTimeout(function () {
           punchline.innerHTML = `<h3>${data.delivery}</h3>`;
           button.disabled = false;
